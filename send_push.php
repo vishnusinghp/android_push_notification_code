7596f0dab852a75fd91a3a341d727d9d08e8dcab
Vishnu Pratap <47685528+vishnusinghp@users.noreply.github.com>
2020-01-05 15:42:22 +0530

    Update send_push.php

--- send_push.php
+++ send_push.php
@@ -1,7 +1,7 @@
       <?php 
       $url = 'https://fcm.googleapis.com/fcm/send';
-        $token = '';// your gcm_token
-        
+        $token[] = 'dOgAavybIro:APA91bERM1wTTWwm_d0GGebbv4xm-T99zd1h1QJHSRRecwya28Og-MlP9MzzDHoym37S_ERTP91kp4eLV-UDxdHJn4Eo2zPUNiwX4OBqjPvdEIlPYa-3a5URcf_eqrtfVEmDVxfZqg88';// your gcm_token
+        $key = 'asdf3343BZQAV';// firebase ids
         $load = array();
         $load['title'] = 'Taxiapp';
         $load['msg'] = 'You have a new ride';
